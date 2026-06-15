efd616346e92dfc28cf7a2591f5d954afd239298
GeorgeFourkas <giorgosfourkas.98@gmail.com>
2026-06-15 20:21:22 +0300

    initial commit

---
+++ src/FilamentNocturneThemeServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nalcom\FilamentNocturneTheme;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentNocturneThemeServiceProvider extends PackageServiceProvider
+{
+
+    public function configurePackage(Package $package): void
+    {
+        $package->name('filament-nocturne-theme');
+    }
+}
